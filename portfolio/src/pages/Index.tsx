f6cfb3776f3d89819c955db25ec5080ccbaa8f2e
pelztheanalyst <adebisipelumi887@gmail.com>
2025-04-30 10:50:52 +0100

    Update Index.tsx

--- portfolio/src/pages/Index.tsx
+++ portfolio/src/pages/Index.tsx
@@ -6,19 +6,26 @@ import Portfolio from "@/components/Portfolio";
 import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import ParticleBackground from "@/components/ParticleBackground";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background">
-      <Navbar />
-      <main>
-        <Hero />
-        <About />
-        <Portfolio />
-        <Skills />
-        <Contact />
-      </main>
-      <Footer />
+    <div className="relative min-h-screen bg-background overflow-x-hidden">
+      {/* Background Particles */}
+      <ParticleBackground />
+
+      {/* Foreground Content */}
+      <div className="relative z-10">
+        <Navbar />
+        <main>
+          <Hero />
+          <About />
+          <Portfolio />
+          <Skills />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 };
